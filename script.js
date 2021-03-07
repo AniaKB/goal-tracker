ae4584bd57738850c67cc880e6e9100ca6c1e7fa
AniaKB <aniakbennett@gmail.com>
2021-03-07 13:28:05 -0500

    updating local files

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 const fs = require('fs');
-console.log(fs)
+
+const goalFile = './goals.json';
+
+try {
+    if (fs.existsSync(goalFile)) {
+    }
+} catch (error) {
+  console.error(error);
+}
+
+try {
+    const data = fs.readFileSync('/goals.json', 'utf8');
+    console.log(data);
+} catch (error) {
+    console.error(error);
+}
