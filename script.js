099b8f1670f79ba708fa26efbac06e69c1bcbb51
AniaKB <aniakbennett@gmail.com>
2021-03-14 13:09:09 -0400

    adding data to JSON file

--- script.js
+++ script.js
@@ -15,3 +15,27 @@ try {
 } catch (error) {
     console.error(error);
 }
+
+
+function add () {
+	document.frm.count.value = parseInt(document.frm.count.value) + 1;
+	addTextBox();
+}
+
+function delete () {
+	document.frm.count.value = parseInt(document.frm.count.value) - 1;
+	removeTextBox();
+}
+
+function addTextBox () {
+	var form = document.frm;
+	form.appendChild(document.createElement('div')).innerHTML = "<table width=\\"40&#37;\\">"
+		+ "<tr><td>Name</td><td><input type=\\"text\\" name=\\"txt\\"></td></tr>"
+		+ "</table>";
+}
+
+function removeTextBox () {
+	var form = document.frm;
+	if (form.lastChild.nodeName.toLowerCase() == 'div')
+		form.removeChild(form.lastChild);
+}
